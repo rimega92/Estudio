bdbc62b9917a955c9fb8c73476e47aef2003b0f7
Ricardo Mesa Gallego <rimesa@bancolombia.com.co>
2022-04-16 16:33:45 -0500

    condicionales en python

---
+++ Python/Inicio/condicionales.py
@@ -0,0 +1,15 @@
+edad = int(input("Introduce tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+print("<><><><><><><><>")
+
+numero = int(input("Introduce un número: "))
+if numero > 5:
+   print("El número es mayor que 5")
+elif numero == 5:
+   print("El número es igual a 5")
+else:
+   print("El número es menor que 5")
