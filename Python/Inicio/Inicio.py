0f9b4a4241232a9e0889ce07e11baa9788fa5a03
Ricardo Mesa Gallego <rimesa@bancolombia.com.co>
2022-04-16 16:17:07 -0500

    Basico Python

---
+++ Python/Inicio/Inicio.py
@@ -0,0 +1,25 @@
+
+#Modo de Importar Librerías
+import math
+
+#Definición de Variables
+a = 1
+b = 2
+pi = math.pi
+
+print("Hello world")
+print(math.sqrt(4))
+print(a + pi)
+
+print("<><><><><><><><><><>")
+print("<><><><><><><><><><>")
+
+# Conversor de Monedas
+usd_trm = 3730
+cop = float(input("Ingrese el valor en COP: "))
+valor_total = cop / usd_trm
+valor_total = round(valor_total, 3)
+print("Tienes: " + str(valor_total) + " dolares")
+
+print("<><><><><><><><><><>")
+print("<><><><><><><><><><>")
