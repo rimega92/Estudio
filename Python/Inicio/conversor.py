2730c69dac591c471b975157a587ead198042887
Ricardo Mesa Gallego <rimesa@bancolombia.com.co>
2022-04-16 16:48:43 -0500

    Conversor V2 con Funciones

--- Python/Inicio/conversor.py
+++ Python/Inicio/conversor.py
@@ -1,5 +1,13 @@
 #convierte pesos mexicanos, argentinos y colombianos a dólares
 
+def conversor(trm,nacionalidad):
+    pesos = input("¿Cuantos pesos " + nacionalidad + " tienes?:      ")
+    pesos = float(pesos)
+    dolares = pesos / trm
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares") 
+
 # """ """ permite crear strings multi lineas
 menu = """
 Bienvenido al conversor de monedas multi país
@@ -15,36 +23,10 @@ Elige una opción:
 opcion = int(input(menu))
 
 if opcion == 1: #pesos mexicanos
-   #pregunto al usuario la cantidad a convertir
-   pesos = input('¿Cuántos pesos mexicanos tienes?: ')
-   #convierto a float para mejor manejo de datos
-   pesos = float(pesos)
-   #escribo el valor del dolar en pesos mexicanos
-   tipo_de_cambio = 21.5
+   conversor(19.50, "Mexicanos")
 elif opcion == 2: #pesos colombianos
-   #pregunto al usuario la cantidad a convertir
-   pesos = input('¿Cuántos pesos colombianos tienes?: ')
-   #convierto a float para mejor manejo de datos
-   pesos = float(pesos)
-   #escribo el valor del dolar en pesos colombianos
-   tipo_de_cambio = 3715.01
+   conversor(3700, "Colombianos")
 elif opcion == 3: #pesos argentinos
-   #pregunto al usuario la cantidad a convertir
-   pesos = input('¿Cuántos pesos argentinos tienes?: ')
-   #convierto a float para mejor manejo de datos
-   pesos = float(pesos)
-   #escribo el valor del dolar en pesos argentinos
-   tipo_de_cambio = 74.44
+   conversor(60, "Argentinos")
 else:  #el usuario escribió algo diferente
    print('Escribe una opción correcta')
-
-
-#hago la conversión
-dolares = pesos / tipo_de_cambio
-#redondeo los dólares a dos decimales
-dolares = round(dolares, 2)
-#convierto el float de dolares a un string
-dolares = str(dolares)
-
-#imprimo el valor de la conversion. Se pueden sumar (concatenar) strings con '+'
-print('Tienes $' + dolares +' dólares')
