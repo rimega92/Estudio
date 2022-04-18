733642d13cebbc575bf5b973dd40589dc32fa020
Ricardo Mesa Gallego <rimesa@bancolombia.com.co>
2022-04-18 17:35:32 -0500

    Recorrer

---
+++ Python/Basico/recorrer.py
@@ -0,0 +1,8 @@
+# Recorrer cada caracter de una frase
+def run():
+    frase = "Esta es una frase de prueba para el ejercicio"
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__ == "__main__":
+    run()
