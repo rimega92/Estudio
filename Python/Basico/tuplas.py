8eef14c8a4f7533705bfd8a5bd9d74962f0c4583
Ricardo Mesa Gallego <rimesa@bancolombia.com.co>
2022-04-18 19:13:38 -0500

    tuplas

---
+++ Python/Basico/tuplas.py
@@ -0,0 +1,9 @@
+# Las tuplas son estructuras de datos inmutables. Es decir, no puedes modificar una tupla a agregando o quitando un valor.
+mi_tupla = ()
+mi_tupla = (1,2,3,3)
+dias_semana = ('lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo')
+print(mi_tupla)
+print(mi_tupla[0])
+print(mi_tupla.count(3))
+
+
